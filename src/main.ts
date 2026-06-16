223e5118e56c323a6d52a06885b61ff57d0a8816
Joel-Tito <jtito.alexander@gmail.com>
2026-06-16 00:59:18 -0500

    deploy frontend nest angular

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: [
+      'http://localhost:4200',
+      'https://frontend-nest-angular.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
 
